2214e44393d73a39a51bde8fa8b48374535980a1
Ly Ung <lyung456@gmail.com>
2019-03-06 00:31:27 -0800

    Homework three initial commit

--- Homework3.js
+++ Homework3.js
@@ -1,30 +1,86 @@
+/* Main function to be called from HTML page */
+function Main() {
+    
+    var boxes = document.getElementsByClassName('box');
+    
+    /* Create organization 1 */
+    PrintDepts(org1_depts, boxes[0]);
+
+    /* Create organization 2 */
+    PrintDepts(org2_depts, boxes[1]);
+}
+
+/* Recursively create unordered list of department based on depts data */
+function PrintDepts(depts, parent) {
+    if(depts.length > 0) {
+        var ul = document.createElement('ul');
+        parent.appendChild(ul);
+    }
+    
+    for(var i=0; i < depts.length; i++) {
+
+        var li = document.createElement('li');
+        li.innerHTML = depts[i].name;
+        ul.appendChild(li);
+
+        if(depts[i].children) {
+            PrintDepts(depts[i].children, li);
+        }
+    }  
+}
+
+/* organization 1 data  */
 const org1_depts = [
     {
-    name: 'accounting',
-    children: [
-        { name: 'accounting payable', children: [] },
-        { name: 'accounting receivable', children: [] },
-        ], 
+        name: 'accounting',
+        children: [
+        { 
+            name: 'accounting payable', 
+            children: [] 
+        },
+        { 
+            name: 'accounting receivable', 
+            children: [] 
+        },
+        ]
     },
     {
         name: 'finance',
-        children: [],I
-    }
+        children: [],
+    },
 ]
 
+/* organization 2 data */
 const org2_depts = [
     {
-    name: 'accounting',
-    children: [
-        { name: 'accounting payable', children: [] },
+        name: 'accounting',
+        children: [
+        { 
+            name: 'accounting payable', 
+            children: [] 
+        },
         {
-         name: 'accounting receivable',
-         children: [{ name: 'cash', children: []}, { name: 'check', children: [] }],
-         }
-    ]
-},
-{
-    name: 'finance',
-    children: [{ name: 'investment', children: {} }],
-},
+            name: 'accounting receivable',
+            children: [
+                { 
+                    name: 'cash', 
+                    children: []
+                }, 
+                { 
+                    name: 'check', 
+                    children: [] 
+                }
+            ],
+        }
+        ]
+    },
+    {
+        name: 'finance',
+        children: [
+            {
+                name: 'investment', 
+                children: {} 
+            }
+        ]
+    }
 ]
